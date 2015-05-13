ac711cbe7d2af54dc92aa958d074879a12839f86
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-13 16:08:51 +0200

    Syntax corrected, model improved and ready to be used for data acquisition and feature extraction

--- model.py
+++ model.py
@@ -1,27 +1,47 @@
+allcards = {}	# Dictionary of all the existing cards. The key is the id, and the value is the instance of the Card class
+
 class Deck:
-	def __init__(self, isConstructed):
+	"""
+	Representation of a deck. It can be constructed or not, complete or not and valid or not
+	Use the method addCard to define the content of the deck
+	"""
+	
+	def __init__(self, isConstructed=True):
+		"""
+		Constructor
+		@param [bool] isConstructed If the deck is an constructed one or not
+		"""
+		self.isConstructed = isConstructed
 		self.__cards = {}
 		self.__nbCards = 0
-		self.isConstructed = isConstructed
-	
-	def addCard(self, card):
-		nbCards += 1
+		self.__nboccMax = 0
+		
+	def addCard(self, card, nbocc=1):
+		"""
+		Add a card in the decks
+		@param card [Card] Instance of the card to add
+		@param nbocc [int] Number of occurences of the card to add
+		"""
+		self.__nbCards += nbocc
+		
 		if card in self.__cards:
-			self.__cards[card] += 1
+			self.__cards[card] += nbocc
 		else:
-			self.__cards[card] = 1
-
+			self.__cards[card] = nbocc
+			
+		if self.__cards[card] > self.__nboccMax :
+			self.__nboccMax = self.__cards[card]
+			
 	@property
 	def cardsMap(self):
 		return self.__cards
 	
 	@property
 	def cardList(self):
-		res = self.__cards.keys()
+		res = []
 		for key in self.__cards:
-			if self.__cards[key] > 1:
-				for i = range( 1, self.__cards[key] ):
-					res.append(key)
+			for i in range( self.__cards[key] ):
+				res.append(key)
 		return res
 
 	@property
@@ -32,14 +52,32 @@ class Deck:
 	def isComplete(self):
 		return self.nbCards == 30
 	
+	@property
+	def isValid(self):
+		return self.isComplete and (not self.isConstructed or self.__nboccMax <= 2)
+	
 	def __hash__(self):
 		return hash(self.cards)
 
 	def __eq__(self, other):
 		return len(set(self.cards.items()) ^ set(other.cards.items())) == 0
 		
-class Card
-	def __init__(self, id, type, manacost, attack, heal, mechanics):
+class Card:
+	"""
+	Representation of a card
+	"""
+	
+	def __init__(self, id, type, manacost, attack=0, heal=0, mechanics=[]):
+		
+		"""
+		Constructor
+		@param id [int] Unique ID of the card
+		@param type [int] Type of the card
+		@param manacost [int] Mana cost of the card
+		@param attack [int] Attack (0 if not a minion)
+		@param heal [int] Heal points (0 if not a minion)
+		@param mechanics [int] List of mechanics ids (can be empty)
+		"""
 		self.id = id
 		self.type = type
 		self.manacost = manacost
