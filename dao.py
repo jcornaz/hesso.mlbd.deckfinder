7e6847e6253a465387a6d0e2388f3a0cbbb276bc
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-05-15 15:05:33 +0200

    Minor improvement for result return

--- dao.py
+++ dao.py
@@ -5,22 +5,25 @@ from model import Deck
 from model import Card
 from model import Types
 
-db_cred = None
-
 class Dao:
 	def __init__(self):
 		self.cards = None
 		self.decks = None
-		
+		self.__con = None
+	
+	def __enter__(self):
 		with open('db_credentials.json') as cred_file:    
 		    db_cred = json.load(cred_file)
-							
+						
 		self.__con = oursql.connect(host=db_cred['host'], user=db_cred['username'], passwd=db_cred['password'], db=db_cred['schema'], port=db_cred['port'])
 		self.__cur = self.__con.cursor(oursql.DictCursor)
 		
-	def __del__(self):
+		return self
+		
+	def __exit__(self, type, value, traceback):
 		if self.__con :
 			self.__con.close()
+			self.__con = None
 	
 	def execute(self, query):
 		self.__cur.execute(query)
@@ -39,11 +42,11 @@ class Dao:
 			
 	def aquireMechanics(self):
 		print "aquiring mechanics..."
-		mechRows = self.execute("SELECT * FROM card_mechanics WHERE card_id IN (SELECT id FROM cards WHERE collectible = 1)")
+		rows = self.execute("SELECT * FROM card_mechanics WHERE card_id IN (SELECT id FROM cards WHERE collectible = 1)")
 				
 		print "processing aquired mechanics..."
 		mechanics = {}
-		for row in mechRows:
+		for row in rows:
 			if row['card_id'] in row['card_id']:
 				row['card_id'].append(row['mechanic_id'])
 			else:
@@ -55,11 +58,11 @@ class Dao:
 		mechanics = self.aquireMechanics()
 		
 		print "aquiring cards..."
-		cardsRows = self.execute("SELECT * FROM cards WHERE collectible = 1 AND type_name != 'Hero' AND mana IS NOT NULL")
+		rows = self.execute("SELECT * FROM cards WHERE collectible = 1 AND type_name != 'Hero' AND mana IS NOT NULL")
 		
 		print "processing aquired cards..."
 		self.cards = {}
-		for row in cardsRows:
+		for row in rows:
 			card = Card(id=row['id'],name=row['name'],type=Dao.strCardTypeToEnum(row['type_name']),isClassCard=not row['klass_id'] is None,rarity=row['rarity_id'],manacost=row['mana'],attack=row['attack'],health=row['health'])
 			if card.id in mechanics:
 				card.mechanics = mechanics[card.id]
@@ -67,20 +70,21 @@ class Dao:
 		
 		return self.cards
 				
-	def aquireDeckList(self):
+	def aquireDeckList(self,modes):
 		if not self.cards:
 			self.aquireCardList()
 
 		print "aquiring decks..."
-		rows = self.execute("SELECT klass_id, cardstring, user_num_matches, user_num_wins FROM decks WHERE id IN (SELECT deck_id FROM constructeds) AND cardstring REGEXP '([0-9]+_[0-9]+,)*([0-9]+_[0-9]+)'")
+		rows = self.execute("SELECT klass_id, cardstring, user_num_matches, user_num_wins FROM decks WHERE id IN (SELECT deck_id FROM constructeds) AND cardstring REGEXP '([0-9]+_[1-2],)*([0-9]+_[1-2])'")
 		
 		print "processing aquired decks..."
 		self.decks = {}
+		res = []
 		for row in rows:
 			if row['cardstring'] in self.decks.keys():
 				deck = self.decks[row['cardstring']]
 			else:
-				deck = Deck(row['klass_id'])
+				deck = Deck(row['klass_id'],True)
 				for strv in string.split(row['cardstring'],','):
 					v = string.split(strv,'_')
 					cardid = int(v[0])
@@ -88,7 +92,8 @@ class Dao:
 					if cardid in self.cards.keys():
 						deck.addCard( self.cards[cardid], nbocc )
 				self.decks[row['cardstring']] = deck
+				res.append(deck)
 			deck.nbConstructedMatches += row['user_num_matches']
 			deck.nbConstructedWins += row['user_num_wins']
-			
-		return self.decks
+		
+		return res
