7db8046ea6f6664a3b4f44cbb3e50d2dfc01f284
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-08 14:45:41 +0200

    dao updated to not get klassless decks

--- dao.py
+++ dao.py
@@ -3,36 +3,65 @@ import json
 import string
 import os.path
 import pickle
+import utils
 from model import Deck
 from model import Card
 from model import Types
 from model import Modes
 
-FILENAME_DB_CREDENTIALS = 'db_credentials.json'
-FILENAME_CARDS_LIST = 'cards_list.dat'
-FILENAME_DECKS_LIST = 'decks_list.dat'
+FILENAME_DB_CREDENTIALS = '../db_credentials.json'
+FILENAME_CARDS_LIST = '../data/cards_list.dat'
+FILENAME_DECKS_LIST = '../data/decks_list.dat'
 
 class Dao:
 	def __init__(self):
-		self.cards = None
-		self.decks = None
+		self.__cards = None
+		self.__decks = None
 		self.__con = None
+		self.__isEntered = False
 	
 	def __enter__(self):
-		with open(FILENAME_DB_CREDENTIALS) as cred_file:    
-		    db_cred = json.load(cred_file)
-
-		self.__con = oursql.connect(host=db_cred['host'], user=db_cred['username'], passwd=db_cred['password'], db=db_cred['schema'], port=db_cred['port'])
-		self.__cur = self.__con.cursor(oursql.DictCursor)
-		
+		self.__isEntered = True
 		return self
 		
 	def __exit__(self, type, value, traceback):
 		if self.__con :
-			self.__con.close()
+			try:
+				self.__con.close()
+			except:
+				pass
 			self.__con = None
+			print "*** Disconnected from SQL server ***"
+			
+		self.__isEntered = False
 	
+	def connect(self):
+		if not self.__isEntered:
+			raise "use the with keyword on the Dao object to be able to connect to the SQL database"
+		else:	
+			with open(FILENAME_DB_CREDENTIALS) as cred_file:    
+			    db_cred = json.load(cred_file)
+			self.__con = oursql.connect(host=db_cred['host'], user=db_cred['username'], passwd=db_cred['password'], db=db_cred['schema'], port=db_cred['port'])
+			self.__cur = self.__con.cursor(oursql.DictCursor)
+			print "*** Connected to SQL server ***"
+		
+	@property
+	def decks(self):
+		if not self.__decks:
+			return self.aquireDeckList()
+		else:
+			return utils.dictValues(self.__decks)
+	
+	@property
+	def cards(self):
+		if not self.__cards:
+			return self.aquireCardList()
+		else:
+			return utils.dictValues(self.__cards)
+			
 	def execute(self, query):
+		if not self.__con:
+			self.connect()
 		self.__cur.execute(query)
 		return self.__cur.fetchall()
 	
@@ -47,16 +76,17 @@ class Dao:
 			return Types.WEAPON
 			
 	def parseCardstring(self,cardstring):
-		if not self.cards:
+		if not self.__cards:
 			self.aquireCardList()
 			
 		res = {}
+		
 		for strv in string.split(cardstring,','):
 			v = string.split(strv,'_')
 			cardid = int(v[0])
 			nbocc = int(v[1])
-			if cardid in self.cards.keys():
-				res[self.cards[cardid]] = nbocc
+			if cardid in self.__cards:
+				res[self.__cards[cardid]] = nbocc
 		
 		return res
 				
@@ -67,7 +97,7 @@ class Dao:
 		print "processing aquired mechanics..."
 		mechanics = {}
 		for row in rows:
-			if row['card_id'] in row['card_id']:
+			if row['card_id'] in mechanics:
 				row['card_id'].append(row['mechanic_id'])
 			else:
 				row['card_id'] = [row['mechanic_id']]
@@ -78,7 +108,7 @@ class Dao:
 		if os.path.isfile(FILENAME_CARDS_LIST):
 			print "loading cards from file..."
 			with open(FILENAME_CARDS_LIST) as file:
-				self.cards = pickle.load(file)
+				self.__cards = pickle.load(file)
 		else:
 			mechanics = self.aquireMechanics()
 			
@@ -86,22 +116,22 @@ class Dao:
 			rows = self.execute("SELECT * FROM cards WHERE collectible = 1 AND type_name != 'Hero' AND mana IS NOT NULL")
 			
 			print "processing aquired cards..."
-			self.cards = {}
+			self.__cards = {}
 			for row in rows:
 				card = Card(id=row['id'],name=row['name'],type=Dao.strCardTypeToEnum(row['type_name']),isClassCard=not row['klass_id'] is None,rarity=row['rarity_id'],manacost=row['mana'],attack=row['attack'],health=row['health'])
 				if card.id in mechanics:
 					card.mechanics = mechanics[card.id]
-				self.cards[card.id] = card
+				self.__cards[card.id] = card
 			
 			print "dumping cards to file..."
 			with open(FILENAME_CARDS_LIST,'w') as file:
-				pickle.dump(self.cards, file)
+				pickle.dump(self.__cards, file,pickle.HIGHEST_PROTOCOL)
 		
-		return self.cards
+		return utils.dictValues(self.__cards)
 	
-	def aquireResults(self,modes=[Modes.ARENA, Modes.RANKED, Modes.CASUAL, Modes.FRIENDLY]):
+	def aquireResults(self,modes=[Modes.RANKED, Modes.CASUAL]):
 		print "aquiring matches victories counts from database..."
-		rows = self.execute( "SELECT d.cardstring, m.mode_id, count(m.id) FROM matches m, match_decks md, decks d WHERE m.id = md.match_id AND d.id = md.deck_id AND d.unique_deck_id IS NOT NULL AND m.mode_id in (" + ','.join(map(str,modes)) + ") AND m.result_id = 1 AND d.cardstring REGEXP '^([0-9]+_[1-9],)*([0-9]+_[1-9])$' GROUP BY d.cardstring, m.mode_id" )
+		rows = self.execute( "SELECT d.cardstring, m.mode_id, count(m.id) FROM matches m, match_decks md, decks d WHERE m.id = md.match_id AND d.id = md.deck_id AND d.klass_id IS NOT NULL AND d.klass_id > 0 AND d.unique_deck_id IS NOT NULL AND m.mode_id in (" + ','.join(map(str,modes)) + ") AND m.result_id = 1 AND d.cardstring REGEXP '^([0-9]+_[1-9],)*([0-9]+_[1-9])$' GROUP BY d.cardstring, m.mode_id" )
 		
 		print "processing aquired results..."
 		arenaWins = {}
@@ -114,41 +144,47 @@ class Dao:
 		
 		return arenaWins, constructedWins
 			
-	def aquireDeckList(self,modes=[Modes.ARENA, Modes.RANKED, Modes.CASUAL, Modes.FRIENDLY]):
-		if not self.cards:
+	def aquireDeckList(self,modes=[Modes.RANKED, Modes.CASUAL]):
+		if not self.__cards:
 			self.aquireCardList()
 
 		if os.path.isfile(FILENAME_DECKS_LIST):
 			print "loading decks from file..."
 			with open(FILENAME_DECKS_LIST) as file:
-				self.decks = pickle.load(file)
+				self.__decks = pickle.load(file)
 		else:
 			arenaWins, constructedsWins = self.aquireResults(modes)
 			
 			print "aquiring decks from database..."
-			rows = self.execute("SELECT d.cardstring, d.klass_id, m.mode_id, count(m.id) FROM matches m, match_decks md, decks d WHERE m.id = md.match_id AND d.id = md.deck_id AND d.unique_deck_id IS NOT NULL AND m.mode_id in (" + ','.join(map(str,modes)) + ") AND d.cardstring REGEXP '^([0-9]+_[1-9],)*([0-9]+_[1-9])$' GROUP BY d.cardstring, m.mode_id")
+			rows = self.execute("SELECT d.cardstring, d.klass_id, m.mode_id, count(m.id) FROM matches m, match_decks md, decks d WHERE m.id = md.match_id AND d.id = md.deck_id AND d.klass_id IS NOT NULL AND d.klass_id > 0 AND d.unique_deck_id IS NOT NULL AND m.mode_id in (" + ','.join(map(str,modes)) + ") AND d.cardstring REGEXP '^([0-9]+_[1-9],)*([0-9]+_[1-9])$' GROUP BY d.cardstring, m.mode_id")
 			
 			print "processing aquired decks..."
-			self.decks = {}
+		 	self.__decks = {}
 			for row in rows:
-				if row['cardstring'] in self.decks.keys():
-					deck = self.decks[row['cardstring']]
+				cards = self.parseCardstring(row['cardstring'])
+				uniqueCardString = Deck.cardstringOfCardsMap(cards)
+				if uniqueCardString in self.__decks:
+					deck = self.__decks[uniqueCardString]
 				else:
 					deck = Deck(row['klass_id'])
-					deck.addAllCards(self.parseCardstring(row['cardstring']))
-					self.decks[row['cardstring']] = deck
+					deck.addCardsMap(cards)
+					self.__decks[uniqueCardString] = deck
 					
 				if row['mode_id'] == Modes.ARENA :
 					deck.nbArenaMatches += row['count(m.id)']
-					if row['cardstring'] in arenaWins.keys():
+					if row['cardstring'] in arenaWins:
 						deck.nbArenaWins += arenaWins[row['cardstring']]
 				else:
 					deck.nbConstructedMatches += row['count(m.id)']
-					if row['cardstring'] in constructedsWins.keys():
+					if row['cardstring'] in constructedsWins:
 						deck.nbConstructedWins += constructedsWins[row['cardstring']]
 			
 			print "dumping decks to file..."
 			with open(FILENAME_DECKS_LIST,'w') as file:
-				pickle.dump(self.decks, file)
+				pickle.dump(self.__decks, file,pickle.HIGHEST_PROTOCOL)
 				
-		return [self.decks[k] for k in self.decks.keys()]
+		return utils.dictValues(self.__decks)
+		
+if __name__ == "__main__":
+	with Dao() as da:
+		da.decks;
