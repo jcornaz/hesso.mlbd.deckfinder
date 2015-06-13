5478133b20c6cbbb5292dcff0a67e04ef183b065
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-13 10:09:31 +0200

    DBScan use improvements and result persistance

--- src/dbscan.py
+++ src/dbscan.py
@@ -4,32 +4,55 @@ import features
 import numpy as np
 import utils
 import sys
+import pickle
 
 def learn(dataset,epsvalue):
-	db = DBSCAN(eps=epsvalue,metric=cosine_distances,algorithm='ball_tree').fit(dataset)
+	db = DBSCAN(eps=epsvalue,min_samples=10,metric=cosine_distances,algorithm='ball_tree').fit(dataset)
 	labels = db.labels_
 	core_samples_mask = np.zeros_like(labels, dtype=bool)
 	core_samples_mask[db.core_sample_indices_] = True
-	nblabels = len(set(labels)) - (1 if -1 in labels else 0)
-	return labels, nblabels, core_samples_mask
+	return set(labels), core_samples_mask
+
+def read_args(args=[]):
+	epsvalue = 1
+	subset_size = sys.maxint
+	file_name = 'dbscan_result.dat'
+	
+	try:
+		if len(args) > 0:
+			epsvalue = int(args[0])
+			if len(args) > 1:
+				if args[1] != "all":					
+					subset_size = int(args[1])
+				if len(args) > 2:
+					file_name = args[2]
+	except Exception:
+		pass
+	
+	return epsvalue, subset_size, file_name
 	
 def main(args=[]):
+	"""
+	Run the dbscan algorithm
+	@param args [startEpsValue=1, randomSubsetSize=all, fileName=dbscan_result.dat]
+	"""
+	epsvalue, subset_size, file_name = read_args(args)
+	
 	dataset = features.load_dataset();
-	if len(args) > 0:
-		epsvalue = int(args[0])
-		if len(args) > 1:
-			dataset = utils.random_subset(dataset,int(args[1]))
-	else:
-		epsvalue = 1
+	if subset_size < sys.maxint:
+		dataset = utils.random_subset(dataset,subset_size)
 
 	print 'learning with eps=' + str(epsvalue) + '...'
-	_, nblabels, _ = learn(dataset, epsvalue)
-	while nblabels <= 1:
+	labels, mask = learn(dataset, epsvalue)
+	while len(labels) <= 1:
 		epsvalue /= 2.0
 		print 'learning with eps=' + str(epsvalue) + '...'
-		_, nblabels, _ = learn(dataset, epsvalue)
+		labels, mask = learn(dataset, epsvalue)
 	
-	print str(nblabels) + " clusters founds"
+	print str(len(labels)) + " clusters founds"
+	print "dumping result in " + file_name
+	with open(file_name,'w') as file:
+		pickle.dump([labels, mask], file, pickle.HIGHEST_PROTOCOL)
 	
 if __name__ == "__main__":
 	main(sys.argv[1:])
