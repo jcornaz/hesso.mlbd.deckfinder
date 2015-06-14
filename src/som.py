c182fe9620e7819439569a719642bb1e6ab1e320
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-14 09:42:35 +0200

    Som script written

--- src/som.py
+++ src/som.py
@@ -1,12 +1,21 @@
 from minisom import MiniSom
 import features    
 import sys
+import pickle
 
+def learn(dataset,sigma=0.3,learning_rate=0.5,nb_iter=10000):
+	som = MiniSom(5,5,4,sigma=sigma,learning_rate=learning_rate)
+	som.train_random(dataset,nb_iter)
+	return som
+	
 def main(args=[]):
-	som = MiniSom(5,5,4,sigma=0.3,learning_rate=0.5) # initialization of 6x6 SOM
-	print "Training..."
-	som.train_random(features.load_dataset(),100) # trains the SOM with 100 iterations
-	print "...ready!"
+	file_name = 'som_result.dat'
+	
+	dataset = features.load_dataset(withComp=True)
+	som = learn(dataset)
+	print "dumping result in " + file_name
+	with open(file_name,'w') as file:
+		pickle.dump(som, file, pickle.HIGHEST_PROTOCOL)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
