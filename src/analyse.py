7e1ec13690c0df1ac4eb09961944f02a555e5d84
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-14 10:27:45 +0200

    Compute of the deck prototype implemented

---
+++ src/analyse.py
@@ -0,0 +1,48 @@
+import features as f
+import model as md
+import numpy as np
+import dao
+import csv
+
+def writeDeckListToCSV(filePath,deckList, deckClasses):
+	with open(filePath+'.csv', 'wb') as csvfile:
+		spamwriter = csv.writer(csvfile, quotechar=';',quoting=csv.QUOTE_MINIMAL)
+		spamwriter.writerow(['class','classeWoW','mana -3 -6 7+', 'health -3 -6 7+', 'attack -3 -6 7+', 'typeDistri-MSW']+["card #"+str(x) for x in range(1,31)])
+		for deck,classe in zip(deckList,deckClasses):
+			l = [c.name for c in deck.cardsList]
+			typeDistri = " ".join("%.4f" % d for d in f.exfe_types(deck))
+			rangeDistri = f.exfe_distri_range(deck)
+			
+			manaDistri = " ".join("%.4f" % d for d in rangeDistri[0:3])
+			attackDistri = " ".join("%.4f" % d for d in rangeDistri[3:6])
+			healthDistri = " ".join("%.4f" % d for d in rangeDistri[6:9])
+			spamwriter.writerow([classe,md.Classes.NAMES[deck.klass],manaDistri,healthDistri, attackDistri,typeDistri]+l)
+			
+def writeDeckPrototypeToCSV(filePath, deckList, deckClasses):
+	classSet = set(deckClasses)
+	features = f.exfe_decks(deckList,True)
+	nb_samples, nb_features = features.shape
+	
+	prototypes = np.zeros(nb_features,len(classSet))
+	winRates = {}
+	matches = {}
+	
+	for i in range(len(classSet)):
+		indexes = filter(lambda index: deckClasses[index] == classSet[i], range(nb_samples))
+		for j in range(nb_features):
+			prototypes[j,i] = np.mean(features[indexes,j])
+			winRates[classSet[i]] = [deck.constructedWinRate for deck in decks[indexes]]
+			matches[classSet[i]] = [deck.nbConstructedMatches for deck in decks[indexes]]
+			
+	with open(filePath + '.csv', 'wb' ) as csvfile:
+		spamwriter = csv.writer(csvfile, quotechar=';',quoting=csv.QUOTE_MINIMAL)
+		for i in range(nb_features):
+			spamwriter.writerow(prototypes[i,:])
+		
+	return prototypes, winRates, matches
+	
+if __name__ == '__main__':
+	with dao.Dao() as da:
+		decks = da.decks
+		
+	writeDeckListToCSV("classeTest",decks,range(len(decks)))
