3dcdead3d83e8f14e9d513f19d7a2cdcce484c8d
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2015-06-15 15:27:49 +0200

    utils.random_sublist added (git merge fails....)

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,22 @@ from time import gmtime, strftime
 def dictValues(dictionnary):
 	return [dictionnary[k] for k in dictionnary.keys()]
 	
+def random_sublist( lst, n ):
+	# Reservoir sampling algorithm
+
+	res = []
+	nbelt = len(lst)
+	
+	for i in range(n):
+		res.append(lst[i])
+	
+	for i in range(n,nbelt):
+		j = rnd.randint(0,i)
+		if j < n:
+			res[j] = lst[i]
+	
+	return res
+	
 def random_subset( matrix, n ):
 	# Reservoir sampling algorithm
 
